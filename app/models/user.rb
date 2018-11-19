f18981124c06457368a25766449f7930fa3666e5
alan glam <alanhaimglam@gmail.com>
2018-11-19 12:48:41 +0200

    added associations to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :bookings
+  # as a client
+  has_many :booked_rooftops, through: :bookings, source: :roof
+  # as an owner
+  has_many :owned_rooftops, class_name: 'Roof'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,45 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_11_19_102347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bookings", force: :cascade do |t|
+    t.integer "price"
+    t.integer "duration"
+    t.bigint "roof_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["roof_id"], name: "index_bookings_on_roof_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "roofs", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.integer "price"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_roofs_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "bookings", "roofs"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "roofs", "users"
 end
