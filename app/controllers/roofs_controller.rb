41b6305c16ea1a538f6d6a52a2179dc6339d955b
moshe sandmann <info@seeiceland.com>
2018-11-19 15:17:23 +0200

    finalized controller

---
+++ app/controllers/roofs_controller.rb
@@ -0,0 +1,71 @@
+class RoofsController < ApplicationController
+  before_action :set_rooftop, only: [:show, :edit, :update, :destroy]
+
+def index
+  @rooftops = Roof.all
+end
+
+def show
+
+end
+
+def edit
+ respond_to do |format|
+      if @rooftop.update(restaurant_params)
+        format.html { redirect_to @rooftop, notice: 'Rooftop was successfully updated.' }
+        format.json { render :show, status: :ok, location: @rooftop }
+      else
+        format.html { render :edit }
+        format.json { render json: @rooftop.errors, status: :unprocessable_entity }
+      end
+    end
+end
+
+def update
+ respond_to do |format|
+      if @rooftop.update(rooftop_params)
+        format.html { redirect_to @rooftop, notice: 'Restaurant was successfully updated.' }
+        format.json { render :show, status: :ok, location: @restaurant }
+      else
+        format.html { render :edit }
+        format.json { render json: @rooftop.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+end
+
+def destroy
+@rooftop.destroy
+    respond_to do |format|
+      format.html { redirect_to rooftops_url, notice: 'Rooftop was successfully destroyed.' }
+      format.json { head :no_content }
+end
+
+def create
+@rooftop = Roof.new(rooftop_params)
+
+    respond_to do |format|
+      if @rooftop.save
+        format.html { redirect_to @restaurant, notice: 'Rooftop was successfully created.' }
+        format.json { render :show, status: :created, location: @rooftop }
+      else
+        format.html { render :new }
+        format.json { render json: @restaurant.errors, status: :unprocessable_entity }
+      end
+    end
+end
+
+def new
+ @rooftop = Roof.new
+end
+
+private
+def set_rooftop
+   @rooftop = Roof.find(params[:id])
+end
+
+ def rooftop_params
+      params.require(:rooftop).permit(:name, :address, :description, :stars, :chef)
+    end
+
+end
---
+++ test/controllers/roofs_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RoofsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
