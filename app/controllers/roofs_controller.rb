df4b5aa68e389fa760e106de6c1f62d82e31d72c
moshe sandmann <info@seeiceland.com>
2018-11-20 16:36:15 +0200

    done

--- app/controllers/roofs_controller.rb
+++ app/controllers/roofs_controller.rb
@@ -1,12 +1,13 @@
 class RoofsController < ApplicationController
-  before_action :set_rooftop, only: [:show, :edit, :update, :destroy]
+  before_action :set_rooftop, only: [ :edit, :update, :destroy]
 
 def index
   @rooftops = Roof.all
 end
 
 def show
-
+  raise
+  @rooftop = Roof.find(params[:id])
 end
 
 def edit
@@ -72,45 +73,5 @@ end
   skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :set_roof, only: [:show, :edit, :update, :destroy]
 
-  def index
-    @roofs = Roof.all
-  end
-
-  def show
-   @roof
-  end
-
-  def new
-    @roof = Roof.new
-  end
-
-  def create
-    @roof = Roof.new(roof_params)
-    @roof.user = current_user
-    if @roof.save!
-      redirect_to roofs_path
-    else
-      render :new
-    end
-  end
-
-  def update
-    @roof.update(roof_params)
-    @roof.save!
-    redirect_to roof_path(@roof)
-  end
-
-  def destroy
-  end
-
-  private
-
-  def set_roof
-    @roof = Roof.find(params[:id])
-  end
-
-  def roof_params
-    params.require(:roof).permit(:name, :location, :price)
-  end
 
 end
