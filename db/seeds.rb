f7b2586c5daa30f075eb847f31f44028afbc0716
Leah Sadoun <leahsadoun@hotmail.fr>
2018-11-22 17:34:54 +0200

    Last change

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,19 +17,68 @@ puts "Created #{User.count} users!"
 
 
 Roof.create!({
-  name: 'Mamilla Hotel Rooftop Lounge and Restaurant',
+  name: 'Dan Hotel Rooftop Lounge',
   location: 'Shlomo ha-Melekh 11, Tel-Aviv',
   price: 5,
   user: random_user,
-  remote_photo_url: 'https://images.unsplash.com/photo-1493246318656-5bfd4cfb29b8?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c4bea0ea38ef1de8e0f23a0a7f9cec8d&auto=format&fit=crop&w=750&q=80'
+  remote_photo_url: 'https://images.unsplash.com/photo-1452509133926-2b180c6d6245?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=af999222be587d4979a4f0de62ce9ef5&auto=format&fit=crop&w=2689&q=80'
 })
-Roof.create!( name: 'Speakeasy', location: 'Rothschild Blvd 24,Tel-Aviv', price: 15, user: random_user, remote_photo_url: 'https://images.unsplash.com/photo-1535935353505-a05186766004?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=128ea8191d6008d104abaf0a0506cdfa&auto=format&fit=crop&w=334&q=80' )
-Roof.create!( name: 'Esperanto Rooftop', location: '14 Beach, Tel-Aviv', price: 25, user: random_user, remote_photo_url: 'https://www.thatsalltrends.com/wp-content/uploads/2017/10/rooftop-_-migliori-rooftop.jpg' )
-Roof.create!( name: 'Beach view', location: 'Eliezer Peri 10, Tel-Aviv', price: 30, user: random_user, remote_photo_url: 'https://images.unsplash.com/photo-1502977601689-3671ddf792b7?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=3ba4a011207ddc96aff2b8dea81538c5&auto=format&fit=crop&w=334&q=80' )
-Roof.create!( name: 'Polihouse', location: 'Nahalat Binyamin St 1,Tel-Aviv', price: 20, user: random_user,  remote_photo_url: 'https://images.unsplash.com/photo-1517356382738-e5f6d7828d84?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=23aed1689a141ce0d5a0b3bfd511abce&auto=format&fit=crop&w=334&q=80' )
-Roof.create!( name: 'Roof face to the beach', location: 'HaYarkon 115, Tel-Aviv', price: 30, user: random_user,  remote_photo_url: 'https://images.unsplash.com/photo-1513279922550-250c2129b13a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=5ab44925abfac230b479589e22b1cc50&auto=format&fit=crop&w=750&q=80' )
-Roof.create!( name: 'Azrieli rooftop', location: 'Derech Menachem Begin 132, Tel-Aviv', price: 20, user: random_user, remote_photo_url: 'https://images.unsplash.com/photo-1498378520120-901cc2e4547e?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d3738c26c7b955003d03ea45f4536015&auto=format&fit=crop&w=753&q=80' )
-Roof.create!( name: 'Benyehuda Street', location: 'HaYarkon 41, Tel-Aviv', price: 30, user: random_user, remote_photo_url: 'https://images.unsplash.com/photo-1483090858014-15e35bb60ea5?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=30103cd1fe3ff7fe3f78446cdd87e901&auto=format&fit=crop&w=333&q=80' )
+
+Roof.create!(
+  name: 'Speakeasy',
+  location: 'Rothschild Blvd 24,Tel-Aviv',
+  price: 15,
+  user: random_user,
+  remote_photo_url: 'https://images.unsplash.com/photo-1479356558243-f9d175b1abea?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=97109f439e438b7c3f7648fdc42afd02&auto=format&fit=crop&w=1489&q=80'
+  )
+
+Roof.create!( name: 'Esperanto Rooftop',
+  location: 'Beach 14, Tel-Aviv',
+  price: 25,
+  user: random_user,
+  remote_photo_url: 'https://www.thatsalltrends.com/wp-content/uploads/2017/10/rooftop-_-migliori-rooftop.jpg'
+  )
+
+Roof.create!( name: 'The Prince',
+  location: 'Eliezer Peri 10, Tel-Aviv',
+  price: 30,
+  user: random_user,
+  remote_photo_url: 'https://images.unsplash.com/photo-1463414689943-2aca18b2242b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f24df5efb21c19a3a6bc5fe2344a620d&auto=format&fit=crop&w=1489&q=80'
+  )
+
+Roof.create!( name: 'Polihouse',
+  location: 'Nahalat Binyamin 1,Tel-Aviv',
+  price: 20,
+  user: random_user,
+  remote_photo_url: 'https://images.unsplash.com/photo-1493246318656-5bfd4cfb29b8?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c4bea0ea38ef1de8e0f23a0a7f9cec8d&auto=format&fit=crop&w=1350&q=80'
+  )
+
+Roof.create!(
+  name: 'Roof face to the beach',
+  location: 'HaYarkon 115, Tel-Aviv',
+  price: 30,
+  user: random_user,
+  remote_photo_url: 'https://www.touristisrael.com/wp-content/uploads/15271610985b06a10ac49b5132187.jpg'
+  )
+
+Roof.create!(
+  name: 'Azrieli rooftop',
+  location: 'Derech Menachem Begin 132, Tel-Aviv',
+  price: 20,
+  user: random_user,
+  remote_photo_url: 'https://az760333.vo.msecnd.net/-/media/property/quirk-hotel/dining/q-rooftop_1-crpd1440x960.jpg?ts=1dc8e147-8919-4454-a731-19b60b6c194e'
+  )
+
+Roof.create!(
+  name: 'Benyehuda Street',
+  location: 'HaYarkon 41, Tel-Aviv',
+  price: 30,
+  user: random_user,
+  remote_photo_url: 'https://zero-media.s3.amazonaws.com/uploads/2017/06/lagare-rooftop-bar.jpg'
+  )
+
+
+
 
 # Roof.create!( name: 'Dan Hotel Rooftop', location: 'Kaufmann 10, Tel-Aviv', price: 150, user: random_user)
 # Roof.create!( name: 'The prince', location: 'Nahalat Binyamin 18, Tel-Aviv', price: 30, user: random_user )
