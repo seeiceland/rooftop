a53531052a72eae4dc86578dea39d1717ce0782f
Leah Sadoun <leahsadoun@hotmail.fr>
2018-11-22 10:39:17 +0200

    datepicker

--- app/javascript/components/datepicker.js
+++ app/javascript/components/datepicker.js
@@ -3,7 +3,7 @@ import flatpickr from 'flatpickr';
 
 const initDatepicker = function () {
   const dateInput = document.getElementById('roofs_date');
-  const bookingDateInput = document.getElementById('booking_period');
+  // const bookingDateInput = document.getElementById('booking_period');
 
   if (dateInput) {
     flatpickr(dateInput, {
@@ -12,12 +12,12 @@ const initDatepicker = function () {
     });
   }
 
-  if (bookingDateInput) {
-    flatpickr(bookingDateInput, {
-      minDate: 'today',
-      dateFormat: 'd-m-Y',
-     });
-  }
+//   if (bookingDateInput) {
+//     flatpickr(bookingDateInput, {
+//       minDate: 'today',
+//       dateFormat: 'd-m-Y',
+//      });
+//   }
 };
 
 export { initDatepicker };
